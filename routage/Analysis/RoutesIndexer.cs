b8bf8d686f8412de7bc196f0cd160cddbf5bfe69
Shrayas <shrayasr@gmail.com>
2016-04-08 12:13:52 +0530

    Initial working project

---
+++ routage/Analysis/RoutesIndexer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace routage.Analysis
+{
+    public class RoutesIndexer
+    {
+        public List<string> Index()
+        {
+            var routeList = new List<string>();
+
+            var slnPath = @"C:\Users\shrayasr\work\code\ixm\";
+            var files = Directory.GetFiles(slnPath, "*.cs", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                var tree = CSharpSyntaxTree.ParseText(File.ReadAllText(file));
+                var syntaxRoot = tree.GetRoot();
+
+                var routeAttrs = syntaxRoot
+                                    .DescendantNodes()
+                                    .OfType<AttributeSyntax>()
+                                    .Where(attr => attr.Name.ToString().ToUpper() == "ROUTE");
+
+                var routes = routeAttrs
+                                .Select(attr => attr.ArgumentList.Arguments)
+                                .Select(attr => attr.ToString())
+                                .Select(route => route.Replace("\"", ""));
+
+                routeList.AddRange(routes);
+            }
+
+            return routeList;
+        }
+    }
+}
---
+++ routage/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using routage.Analysis;
+using System.Web.Mvc;
+
+namespace routage.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("~/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("routes")]
+        public ActionResult GetRoutes()
+        {
+            var routesIndexer = new RoutesIndexer();
+            return Json(routesIndexer.Index(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
